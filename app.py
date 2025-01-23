6bafbb6f73763f221d8b2c2583b679f1c7298e1a
Abdullah <abdullahhamid2395@gmail.com>
2025-01-23 23:16:43 +0500

    a step towards integrating models

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask,render_template
+import json
 app=Flask(__name__)
 @app.route("/",methods=["GET"])
 def niche():
-    return render_template("niche.html")
+    with open("products.json") as f:
+        data=json.load(f)
+    products=data.values()
+    return render_template("products.html",products=products)
+@app.route("/sign_up",methods=["GET"])
+def sign_up():
+    return render_template("Signup.html")
 if __name__=="__main__":
     app.run(debug=True)
